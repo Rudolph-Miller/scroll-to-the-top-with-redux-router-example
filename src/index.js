2336dddb9fd0ca933cf55a31aa88d633cdd08c38
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-01-25 21:15:02 +0900

    Step1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
-import { render } from 'react-dom'
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { connect, Provider } from 'react-redux';
+import { IndexRoute, Route, Redirect, Link } from 'react-router';
+import { reduxReactRouter, routerStateReducer, ReduxRouter } from 'redux-router';
+import createHistory from 'history/lib/createHashHistory';
+
+const Styles = {
+  linkContainer: {
+    marginTop: '1000px'
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        {this.props.children}
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('app'));
+class Hoge extends Component {
+  render() {
+    return (
+      <div>
+        <div>Hoge</div>
+        <div style={Styles.linkContainer}>
+          <Link to="/fuga">
+            To Fuga
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Fuga extends Component {
+  render() {
+    return (
+      <div>
+        <div>Fuga</div>
+        <div style={Styles.linkContainer}>
+          <Link to="/hoge">
+            To Hoge
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+const routes = (
+  <Route>
+    <Redirect path="/" to="hoge" />
+    <Route path="/" component={App}>
+      <Route path="hoge" component={Hoge} />
+      <Route path="fuga" component={Fuga} />
+    </Route>
+  </Route>
+);
+
+const reducer = combineReducers({
+  router: routerStateReducer
+});
+
+const store = reduxReactRouter({routes, createHistory})(createStore)(reducer);
+
+class Root extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ReduxRouter />
+      </Provider>
+    );
+  }
+}
+
+render(<Root />, document.getElementById('app'));
